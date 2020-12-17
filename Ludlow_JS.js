1f778b5d7def8da3a2636a485d15112262f60f7d
josecarlos504 <61512211+josecarlos504@users.noreply.github.com>
2020-12-17 12:45:43 +0000

    Jquery

--- Ludlow_JS.js
+++ Ludlow_JS.js
@@ -1,4 +1,4 @@
-//==========================================================================================================================================
+//Created by Esteban Araujo-Fervenza 
 //Responsive Menu JS. The window.onload deals with the null error from trying to operate in a DOM before being loaded
 window.onload = function(){
 	const menu = document.querySelector(".menu-list");
@@ -14,4 +14,61 @@ window.onload = function(){
 	}
 }
 
-//========================================================================================================================================
+//=======================================================================================================================
+
+// Created by Jose Carlos Kuzolitz Garcia - x20164840
+
+//Jquery with validates the entry in the Contact.html Form
+
+//Based on the following tutorial - (https://www.youtube.com/watch?v=xNSQ3i-BWMo)
+
+$(function(){	
+
+	
+	$("#send-message").validate({
+
+		/* Validation Rules */
+		rules:{
+			/* Name Field is required */
+			fname:{
+				required: true
+			},
+			/* Just accepts numbers max 10 length (not required) */
+			phone:{
+				digits: true,
+			},
+			/* Email field is required and it inspects whether it has the standard layout or not */
+			email:{
+				required: true,
+				email: true
+			},
+			/* textArea is required, in order to send a request just when data is input */
+			textArea:{
+				required:true
+			}
+		},
+
+		/* Error Messages (it uses the "Name" of the element) */
+		messages:{
+			fname:{
+				required: 'Please, What is your name?'
+			},
+			
+			phone:{
+				digits: 'Only numbers.',
+			},
+
+			email:{
+				required: 'Please, enter an email address',
+				email: 'Please, enter a <em>valid</em> email address.'
+			},
+
+			textArea:{
+				required: "Don't you want to leave a message?"
+			}
+
+		}
+
+	});
+
+});
