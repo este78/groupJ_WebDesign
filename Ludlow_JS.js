ec29b1669335fac525ff893e39a833cadde0f27e
Sami-cod <74678223+Sami-cod@users.noreply.github.com>
2020-12-18 21:20:45 +0000

    Update Ludlow_JS.js

--- Ludlow_JS.js
+++ Ludlow_JS.js
@@ -72,3 +72,13 @@ $(function(){
 	});
 
 });
+
+/*--------------------------------------------------------------*/
+
+let carts = document.querySelectorAll('.add-cart');
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        console.log("added to cart");
+    })
+}
